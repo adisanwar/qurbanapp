b05098273723954261a0b96469a3e622fce63a4a
synysterRev <69097348+synysterR3v@users.noreply.github.com>
2023-10-10 17:08:54 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+import 'package:qurban_app/ui/home.dart';
+//package letak folder Anda
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        // Remove the debug banner
+        debugShowCheckedModeBanner: false,
+        title: 'Kindacode.com',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+        ),
+        home: const HomePage());
+  }
+}
